4103831536324604e39baab660549a6cb1b70536
mavsm <mathias.vsm@gmail.com>
2019-12-10 15:03:24 -0200

    Add android import to config

--- config.py
+++ config.py
@@ -3,6 +3,9 @@ def can_build(env, platform):
     return platform == "iphone"
 
 def configure(env):
+    if (env['platform'] == 'android'):
+        env.android_add_dependency("implementation 'com.revenuecat.purchases:purchases:3.0.3'")
+        env.android_add_java_dir("android/src/")
     if env['platform'] == "iphone":
         env.Append(FRAMEWORKPATH=['ios/lib'])
         env.Append(LINKFLAGS=['-ObjC', '-framework', 'Purchases'])
