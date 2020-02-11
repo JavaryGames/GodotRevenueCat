747d79ad23993e53fb8bcd851493f87398224028
mavsm <mathias.vsm@gmail.com>
2020-02-11 15:52:21 -0200

    Add .conf files

--- config.py
+++ config.py
@@ -3,10 +3,11 @@ def can_build(env, platform):
     return platform == "android" or platform == "iphone"
 
 def configure(env):
-    if (env['platform'] == 'android'):
+    if env['platform'] == 'android':
         return
-        env.android_add_dependency("implementation 'com.revenuecat.purchases:purchases:3.0.3'")
-        env.android_add_java_dir("android/src/")
-    if env['platform'] == "iphone":
+        env.android_add_dependency("implementation 'com.revenuecat.purchases:purchases:2.4.1'")
+        # env.android_appattributes_chunk += ' tools:replace="appComponentFactory" '
+        env.android_add_java_dir("android")
+    elif env['platform'] == "iphone":
         env.Append(FRAMEWORKPATH=['ios/lib'])
         env.Append(LINKFLAGS=['-ObjC', '-framework', 'Purchases'])
